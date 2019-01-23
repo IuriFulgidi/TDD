ab935988ad22ec84393b2cb97706dedb35b337fb
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 09:13:39 +0100

    Creazione di due test per rtovare il massimo tra 2 numeri

--- Confronto_Tests/TDD_Tests.cs
+++ Confronto_Tests/TDD_Tests.cs
@@ -7,8 +7,25 @@ namespace Confronto_Tests
     public class TDD_Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMax()
         {
+            int num1=4;
+            int num2=10;
+            int max_expected = 10;
+
+            int max_calculated = TDD_Library.TDD.Max(num1, num2);
+            Assert.AreEqual(max_expected, max_calculated);
+        }
+
+        [TestMethod]
+        public void TestMax1()
+        {
+            int num1 = 10;
+            int num2 = 4;
+            int max_expected = 10;
+
+            int max_calculated = TDD_Library.TDD.Max(num1, num2);
+            Assert.AreEqual(max_expected, max_calculated);
         }
     }
 }
--- TDD_Library/TDD.cs
+++ TDD_Library/TDD.cs
@@ -8,5 +8,15 @@ namespace TDD_Library
 {
     public static class TDD
     {
+        public static int Max(int num1, int num2)
+        {
+            int ris = num1;
+            if(num2>num1)
+            {
+                ris = num2;
+            }
+            return ris;
+        }
+
     }
 }
